666fb1d63d0f5b58d315f97dec72fca75dccbebb
nykw <nykw196884@gmail.com>
2021-03-18 04:11:05 +0900

    18-1. ユーザーモデルの構築

--- unit_4/lesson_18/recipe_app/repl.js
+++ unit_4/lesson_18/recipe_app/repl.js
@@ -1,60 +1,26 @@
-const mongoose = require("mongoose"),
-  Subscriber = require("./models/subscriber"),
-  Course = require("./models/course");
-var testCourse, testSubscriber;
-mongoose.connect(
-  "mongodb://localhost:27017/recipe_db",
-  { useNewUrlParser: true }
-);
-mongoose.Promise = global.Promise;
-Subscriber.remove({})
-  .then(items => console.log(`Removed ${items.n} records!`))
-  .then(() => {
-    return Course.remove({});
-  })
-  .then(items => console.log(`Removed ${items.n} records!`))
-  .then(() => {
-    return Subscriber.create({
-      name: "Jon",
-      email: "jon@jonwexler.com",
-      zipCode: "12345"
-    });
-  })
-  .then(subscriber => {
-    console.log(`Created Subscriber: ${subscriber.getInfo()}`);
-  })
-  .then(() => {
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const Subscriber = require('./models/subscriber');
+
+mongoose.connect('mongodb://localhost:27017/recipe_db', { useNewUrlParser: true });
+let testUser;
+User.create({
+  name: {
+    first: 'Jon',
+    last: 'Wexler'
+  },
+  email: 'jon@jonwexler.com',
+  password: 'pass123'
+})
+  .then(user => {
+    testUser = user;
     return Subscriber.findOne({
-      name: "Jon"
+      email: user.email
     });
   })
   .then(subscriber => {
-    testSubscriber = subscriber;
-    console.log(`Found one subscriber: ${subscriber.getInfo()}`);
+    testUser.subscribedAccount = subscriber;
+    testUser.save()
+      .then(user => console.log('user updated'))
   })
-  .then(() => {
-    return Course.create({
-      title: "Tomato Land",
-      description: "Locally farmed tomatoes only",
-      zipCode: 12345,
-      items: ["cherry", "heirloom"]
-    });
-  })
-  .then(course => {
-    testCourse = course;
-    console.log(`Created course: ${course.title}`);
-  })
-  .then(() => {
-    testSubscriber.courses.push(testCourse);
-    testSubscriber.save();
-  })
-  .then(() => {
-    return Subscriber.populate(testSubscriber, "courses");
-  })
-  .then(subscriber => console.log(subscriber))
-  .then(() => {
-    return Subscriber.find({
-      courses: mongoose.Types.ObjectId(testCourse._id)
-    });
-  })
-  .then(subscriber => console.log(subscriber));
+  .catch(err => console.log(err.message));
