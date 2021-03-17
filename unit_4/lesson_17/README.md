79e9b61cd6bda5cefad7e45d2175b39af77ed08f
nykw <nykw196884@gmail.com>
2021-03-17 15:32:24 +0900

    17-1. モデルにバリデーションを追加する。

--- unit_4/lesson_17/README.md
+++ unit_4/lesson_17/README.md
@@ -1,1 +1,42 @@
 # メモ
+
+## レッスンの概要
+
+- モデルに妥当性検証を追加する
+- モデルに、静的メソッドとインスタンスメソッドを作る
+- モデルをREPLでテストする
+- 複数のモデルにデータの関連付けを実装する
+
+## その他
+
+- Mongooseにおけるスキーマ型
+
+  - 知っておくべきスキーマ型
+
+    - `String`
+    - `Date`
+    - `Array`
+
+      例： `[String]`
+
+    - `Mixed`
+
+      この型は、キーと値のペアをモデルに保存するので、JavaScriptのオブジェクトに最も近い型です。
+      `mongoose.Schema.Types.Mixed` と指定します。
+
+    - `ObjectId`
+
+      MongoDBデータベースの各ドキュメントにある ObjectId 値と同じく、この型もオブジェクトの参照です。
+      特に、モデルを他のモデルと関連付けるときは、この型が重要です。
+      `mongoose.Schema.Types.ObjectId` と指定します。
+
+- [Mongooseのインスタンスメソッド](https://mongoosejs.com/docs/guide.html#methods)
+
+- Mongooseのクエリメソッド
+
+  | クエリ | 説明 |
+  | --- | :--- |
+  | `find` | クエリのパラメータにマッチするレコードの配列を返す。 |
+  | `findOne` | 一つのレコードを返す。 |
+  | `findById` | データベースへのクエリを ObjectId で行う。 |
+  | `remove` | データベースにあるドキュメントを削除できる。 |
--- unit_4/lesson_17/recipe_app/models/subscriber.js
+++ unit_4/lesson_17/recipe_app/models/subscriber.js
@@ -1,10 +1,34 @@
 "use strict";
 
-const mongoose = require("mongoose"),
-  subscriberSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    zipCode: Number
-  });
+const mongoose = require("mongoose");
+const subscriberSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  zipCode: {
+    type: Number,
+    min: [10000, 'Zip code too short'],
+    max: 99999
+  }
+});
+
+// 購読者のフルネームを取得する
+subscriberSchema.methods.getInfo = function () {
+  return `Name:${this.name} Email:${this.email} Zip Code:${this.zipCode}`;
+};
+
+// 同じZIPコードを持つ購読者を見つける
+subscriberSchema.methods.findLocalSubscribers = function () {
+  return this.model('Subscriber')
+    .find({ zipCode: this.zipCode })
+    .exec();
+};
 
 module.exports = mongoose.model("Subscriber", subscriberSchema);
