bd59ae3ae8e679e9496e19ef55dbd889300e1dd7
nykw <nykw196884@gmail.com>
2021-03-21 15:38:24 +0900

    27-2. モーダル経由のコース参加

--- unit_6/lesson_27/recipe_app/public/js/recipeApp.js
+++ unit_6/lesson_27/recipe_app/public/js/recipeApp.js
@@ -10,12 +10,35 @@ $(document).ready(() => {
 						<span class="course-title">
 							${course.title}
 						</span>
+						<button class='button ${course.joined ? "joined-button" : "join-button"}' data-id="${course._id}">
+							${course.joined ? "Joined" : "Join"}
+						</button>
 						<div class="course-description">
 							${course.description}
 						</div>
 					</div>`
         );
       });
+    }).then(() => {
+      addJoinButtonListener();
     });
   });
 });
+
+let addJoinButtonListener = () => {
+  $(".join-button").click(event => {
+    let $button = $(event.target),
+      courseId = $button.data("id");
+    $.get(`/api/courses/${courseId}/join`, (results = {}) => {
+      let data = results.data;
+      if (data && data.success) {
+        $button
+          .text("Joined")
+          .addClass("joined-button")
+          .removeClass("join-button");
+      } else {
+        $button.text("Try again");
+      }
+    });
+  });
+};
