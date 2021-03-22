e5d78cd1f0f310f933517ed346106033eac9bbd5
nykw <nykw196884@gmail.com>
2021-03-22 23:13:02 +0900

    29-1. 経路の再編成

--- unit_6/lesson_29/README.md
+++ unit_6/lesson_29/README.md
@@ -1,1 +1,40 @@
 # 演習：APIを実装する
+
+## アプリケーションの構造
+
+```text
+confetti_cuisine/
+  models/
+    user.js
+    course.js
+    subscriber.js
+  views/
+    users/
+      index.ejs
+      new.ejs
+      show.ejs
+      edit.ejs
+    courses/
+      index.ejs
+      new.ejs
+      show.ejs
+      edit.ejs
+    subscribers/
+      index.ejs
+      new.ejs
+      show.ejs
+      edit.ejs
+  controllers/
+    usersController.js
+    coursesController.js
+    subscriberController.js
+  public/
+    images/
+    js/
+    css/
+  routes/
+    userRoutes.js
+    courseRoutes.js
+    subscriberRoutes.js
+  main.js
+```
--- unit_6/lesson_29/confetti_cuisine/main.js
+++ unit_6/lesson_29/confetti_cuisine/main.js
@@ -3,12 +3,7 @@
 const express = require("express"),
   layouts = require("express-ejs-layouts"),
   app = express(),
-  router = express.Router(),
-  homeController = require("./controllers/homeController"),
-  errorController = require("./controllers/errorController"),
-  subscribersController = require("./controllers/subscribersController.js"),
-  usersController = require("./controllers/usersController.js"),
-  coursesController = require("./controllers/coursesController.js"),
+  router = require('./routes/index'),
   mongoose = require("mongoose"),
   methodOverride = require("method-override"),
   passport = require("passport"),
@@ -69,55 +64,7 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get("/", homeController.index);
-
-router.get("/users", usersController.index, usersController.indexView);
-router.get("/users/new", usersController.new);
-router.post(
-  "/users/create",
-  usersController.validate,
-  usersController.create,
-  usersController.redirectView
-);
-router.get("/users/login", usersController.login);
-router.post("/users/login", usersController.authenticate);
-router.get("/users/logout", usersController.logout, usersController.redirectView);
-router.get("/users/:id/edit", usersController.edit);
-router.put("/users/:id/update", usersController.update, usersController.redirectView);
-router.get("/users/:id", usersController.show, usersController.showView);
-router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-
-router.get("/subscribers", subscribersController.index, subscribersController.indexView);
-router.get("/subscribers/new", subscribersController.new);
-router.post(
-  "/subscribers/create",
-  subscribersController.create,
-  subscribersController.redirectView
-);
-router.get("/subscribers/:id/edit", subscribersController.edit);
-router.put(
-  "/subscribers/:id/update",
-  subscribersController.update,
-  subscribersController.redirectView
-);
-router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
-router.delete(
-  "/subscribers/:id/delete",
-  subscribersController.delete,
-  subscribersController.redirectView
-);
-
-router.get("/courses", coursesController.index, coursesController.indexView);
-router.get("/courses/new", coursesController.new);
-router.post("/courses/create", coursesController.create, coursesController.redirectView);
-router.get("/courses/:id/edit", coursesController.edit);
-router.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
-router.get("/courses/:id", coursesController.show, coursesController.showView);
-router.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
-
-router.use(errorController.pageNotFoundError);
-router.use(errorController.internalServerError);
-
+// Routingは /routes/index.js からのみ読み込む
 app.use("/", router);
 
 app.listen(app.get("port"), () => {
